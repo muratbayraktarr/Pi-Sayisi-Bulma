415389e7443997b2a9cfb6e6ab226d8c75d3bd1e
Murat Bayraktar <210205006@ostimteknik.edu.tr>
2023-08-15 00:26:08 +0300

    Pi Sayısı Bulma Uygulaması

---
+++ PiSayisiEldeEt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PiApproximation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int totalPoints = 1000000; 
+            int insideCircle = 0; 
+
+            for (int i = 0; i < totalPoints; i++)
+            {
+                double x = random.NextDouble(); 
+                double y = random.NextDouble();
+
+                
+                if (x * x + y * y <= 1)
+                {
+                    insideCircle++;
+                }
+            }
+
+            double piApproximation = ((double)insideCircle / totalPoints) * 4;
+            Console.WriteLine("Ödev 1");
+            Console.WriteLine("Yaklaşık Pi Değeri: " + piApproximation);
+            Console.ReadKey();
+        }
+    }
+}
